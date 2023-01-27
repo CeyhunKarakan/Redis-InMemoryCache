6c968460344fa06a02466483484b5b18b988730d
CeyhunKarakan <33041044+CeyhunKarakan@users.noreply.github.com>
2023-01-27 10:39:59 +0300

    Add project files.

---
+++ RedisTest/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace RedisTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            if (!_memoryCache.TryGetValue("time", out string timeCache))
+            {
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+                options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+                options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+
+                _memoryCache.Set<string>("time", DateTime.Now.ToString(), options);
+            }
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            _memoryCache.TryGetValue("time", out string timeCache);
+            ViewBag.Time = timeCache;   
+
+            return View();
+        }
+    }
+}
