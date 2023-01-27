10296bcccad05ef0f20fc2f7052f1c06bcdfe1e6
CeyhunKarakan <33041044+CeyhunKarakan@users.noreply.github.com>
2023-01-27 11:35:11 +0300

    RegisterPostEvictionCallback method

--- RedisTest/Controllers/ProductController.cs
+++ RedisTest/Controllers/ProductController.cs
@@ -16,10 +16,15 @@ namespace RedisTest.Controllers
             if (!_memoryCache.TryGetValue("time", out string timeCache))
             {
                 MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
-                options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+                options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
 
-                options.SlidingExpiration = TimeSpan.FromSeconds(10);
+                //options.SlidingExpiration = TimeSpan.FromSeconds(10);
                 options.Priority = CacheItemPriority.High;
+
+                options.RegisterPostEvictionCallback((key, value, reason, state) =>
+                {
+                    _memoryCache.Set("callback", $"{key}->{value} => reason:{reason}");
+                });
 
                 _memoryCache.Set<string>("time", DateTime.Now.ToString(), options);
             }
@@ -30,6 +35,8 @@ namespace RedisTest.Controllers
         public IActionResult Show()
         {
             _memoryCache.TryGetValue("time", out string timeCache);
+            _memoryCache.TryGetValue("callback", out string callback);
+            ViewBag.callback = callback;
             ViewBag.Time = timeCache;   
 
             return View();
